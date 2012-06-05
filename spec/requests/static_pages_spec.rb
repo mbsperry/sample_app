10f1438c1361bb4eb9c40141ffc773740df7a4bd
Matthew Sperry <mbsperry@gmail.com>
2012-06-05 00:02:22 -0700

    Test working again, method in book fails

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,26 +7,29 @@ describe "StaticPages" do
 	describe "Home page" do
     before { visit root_path }
 
-		it { should have_selector('h1', text: 'Sample App') }
-    it { should have_selector('title', text: full_title('')) }
-    it { should_not have_select 'title', text: 'Home' }
+		it { should have_selector('h1', :text => 'Sample App') }
+		it { should have_selector('title', text: "Ruby on Rails Tutorial") }
+    it { should_not have_select 'title', text: '| Home' }
   end
 
 	describe "Help page" do
     before { visit help_path }
 		it { should have_selector('h1', :text => 'Help') }
-		it { should have_selector('title', :text => full_title("Help"))  }
+		it { should have_selector('title',
+											:text => " | Help") }
 	end
 
 	describe "About Page" do
     before { visit about_path }
 		it { should have_selector('h1', :text => 'About Us') }
-		it { should have_selector('title', :text => full_title(" About Us")) }
+		it { should have_selector('title',
+											:text => " | About Us") }
 	end
 
   describe "Contact Page" do
-    before { visit contact_path }
-		it { should have_selector('h1', :text => 'Contact') }
-		it { should have_selector('title', :text => full_title("Contact")) }
+    before { visit help_path }
+		it { should have_selector('h1', :text => 'Help') }
+		it { should have_selector('title',
+											:text => " | Help") }
 	end
 end
