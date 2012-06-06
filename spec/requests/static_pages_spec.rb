433de906df6e24a12dbfefd65a9ca764e8017f4d
Matthew Sperry <mbsperry@gmail.com>
2012-06-05 21:52:18 -0700

    Fixed Rspec issues

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,34 +2,31 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-  subject { page }
+  subject { page.body }
 
 	describe "Home page" do
     before { visit root_path }
 
 		it { should have_selector('h1', :text => 'Sample App') }
-		it { should have_selector('title', text: "Ruby on Rails Tutorial") }
-    it { should_not have_select 'title', text: '| Home' }
+		it { should have_selector('title', text: full_title(''))}
+    it { should_not have_selector( 'title', text: '| Home') }
   end
 
 	describe "Help page" do
     before { visit help_path }
 		it { should have_selector('h1', :text => 'Help') }
-		it { should have_selector('title',
-											:text => " | Help") }
+		it { should have_selector('title', text: full_title('Help'))}
 	end
 
 	describe "About Page" do
     before { visit about_path }
 		it { should have_selector('h1', :text => 'About Us') }
-		it { should have_selector('title',
-											:text => " | About Us") }
+		it { should have_selector('title', text: full_title('About'))}
 	end
 
   describe "Contact Page" do
-    before { visit help_path }
-		it { should have_selector('h1', :text => 'Help') }
-		it { should have_selector('title',
-											:text => " | Help") }
+    before { visit contact_path }
+		it { should have_selector('h1', :text => 'Contact') }
+		it { should have_selector('title', text: full_title('Contact'))}
 	end
 end
